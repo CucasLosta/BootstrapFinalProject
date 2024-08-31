db673ec3f48b349a531d33878e5523271a08e6df
Lucas Costa <lucascosta830@gmail.com>
2024-08-31 17:42:41 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// Changing styles of the navigation bar when scrolling
+let nav = document.querySelector("nav");
+
+const navScrollChange=()=>{
+    let logo = document.getElementsByClassName("logo");
+    let navLink = document.getElementsByClassName("rightNavLink");
+    
+    if(window.scrollY > 200){
+        nav.classList.add("scrolledNav");
+        logo[0].classList.add("scrolledLogo");
+
+        for(let i = 0; i < navLink.length; i++){
+            navLink[i].classList.add("scrolledLink");
+        }
+    
+    }else{
+        document.querySelector("nav").classList.remove("scrolledNav");
+        logo[0].classList.remove("scrolledLogo");
+
+        for(let i = 0; i < navLink.length; i++){
+            navLink[i].classList.remove("scrolledLink");
+        }
+    }
+}
+
+window.addEventListener("scroll", navScrollChange);
+
+// Set the upper padding of the sections as the navigation bar height
+

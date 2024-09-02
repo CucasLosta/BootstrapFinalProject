b919f11d5cd231ade3fee9e0dcb7464f15683f21
Lucas Costa <lucascosta830@gmail.com>
2024-09-02 19:38:10 -0300

    Added Projects section

--- script.js
+++ script.js
@@ -27,3 +27,41 @@ window.addEventListener("scroll", navScrollChange);
 
 // Set the upper padding of the sections as the navigation bar height
 
+
+// Function to make projects visible
+function makeVisibleOnScroll(element) {
+    // Get element y position
+    const elementPosition = element.getBoundingClientRect().top + window.scrollY;
+
+    // Function executed when scrolling
+    function onScroll() {
+        // Get current scroll position
+        const scrollPosition = window.scrollY + window.innerHeight/8;
+
+        // Verify if scroll position has reached the element's position
+        if (scrollPosition >= elementPosition) {
+            element.style.transition = 'opacity 1s ease-in-out, transform 1s ease-in-out';
+            element.style.opacity = 1;
+            element.style.transform = 'translateY(-30px)';
+
+            window.removeEventListener('scroll', onScroll);
+        }
+    }
+
+    // Initially hide element
+    element.style.opacity = 0;
+
+    // Agregar el event listener para el scroll
+    window.addEventListener('scroll', onScroll);
+}
+
+let project = document.getElementsByClassName('projectContainer');
+
+for(let i = 0; i < project.length; i++){
+    makeVisibleOnScroll(project[i]);
+}
+
+// Event for the "Zorpulate button"
+
+btnZorpulate = document.getElementById("btnZorpulate");
+btnZorpulate.addEventListener('click',()=>{alert("Para zorpular debe tener su plenixo activado.")})
